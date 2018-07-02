890280c31b0f05ca650f0cb68829680a468df30a
Alex Deiter <alex.deiter@gmail.com>
2018-07-02 13:04:11 +0000

    NEX-17706 Sync NexentaStor5 Cinder driver: Juno NFS/iSCSI with Master

--- cinder/volume/drivers/nexenta/ns5/nfs.py
+++ cinder/volume/drivers/nexenta/ns5/nfs.py
@@ -31,7 +31,7 @@ from cinder.volume.drivers.nexenta import options
 from cinder.volume.drivers.nexenta import utils
 from cinder.volume.drivers import nfs
 
-VERSION = '1.6.8'
+VERSION = '1.6.9'
 LOG = logging.getLogger(__name__)
 BLOCK_SIZE_MB = 1
 
@@ -47,7 +47,6 @@ class NexentaNfsDriver(nfs.NfsDriver):
                 Added abandoned volumes and snapshots cleanup.
         1.3.0 - Failover support.
         1.4.0 - Migrate volume support and new NEF API calls.
-        1.5.0 - Revert to snapshot support.
         1.6.0 - Get mountPoint from API to support old style mount points.
                 Mount and umount shares on each operation to avoid mass
                 mounts on controller. Clean up mount folders on delete.
@@ -59,6 +58,7 @@ class NexentaNfsDriver(nfs.NfsDriver):
         1.6.6 - Destroy unused snapshots after deletion of it's last clone.
         1.6.7 - Fixed volume migration for HA environment.
         1.6.8 - Added deferred deletion for snapshots.
+        1.6.9 - Fixed race between volume/clone deletion.
     """
 
     driver_prefix = 'nexenta'
@@ -521,7 +521,7 @@ class NexentaNfsDriver(nfs.NfsDriver):
             self.nef.delete(url)
         except exception.NexentaException as ex:
             err = utils.ex2err(ex)
-            if 'Failed to destroy snap' in err['message']:
+            if err['code'] == 'EEXIST':
                 params = {'parent': path}
                 url = 'storage/snapshots?%s' % (
                     urllib.parse.urlencode(params))
@@ -540,15 +540,14 @@ class NexentaNfsDriver(nfs.NfsDriver):
                     url = 'storage/filesystems/%s/promote' % (
                         urllib.parse.quote_plus(clone))
                     self.nef.post(url)
-                    path = self._get_dataset_name(volume)
-                    params = {
-                        'force': 'true',
-                        'snapshots': 'true'
-                    }
-                    url = 'storage/filesystems/%s?%s' % (
-                        urllib.parse.quote_plus(path),
-                        urllib.parse.urlencode(params))
-                    self.nef.delete(url)
+                params = {
+                    'force': 'true',
+                    'snapshots': 'true'
+                }
+                url = 'storage/filesystems/%s?%s' % (
+                    urllib.parse.quote_plus(path),
+                    urllib.parse.urlencode(params))
+                self.nef.delete(url)
             else:
                 raise ex
 
